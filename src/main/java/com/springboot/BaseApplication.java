90d84a9fd3cabac03f13d24a55f367d2142d6e31
sheep <2369648034@qq.com>
2019-11-25 21:57:32 +0800

    version 1.0

--- src/main/java/com/springboot/BaseApplication.java
+++ src/main/java/com/springboot/BaseApplication.java
@@ -1,4 +1,4 @@
-package com.springboot.base;
+package com.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/springboot/base/BaseApplicationTests.java
+++ src/test/java/com/springboot/base/BaseApplicationTests.java
@@ -1,13 +1,31 @@
 package com.springboot.base;
 
+import com.springboot.Entity.Person;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 @SpringBootTest
 class BaseApplicationTests {
 
     @Test
-    void contextLoads() {
+    void contextLoads() throws IOException {
+        InputStream in= Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory=new SqlSessionFactoryBuilder().build(in);
+        SqlSession session=sqlSessionFactory.openSession();
+        Person person =session.selectOne("test.FindPersonById", 2);
+        System.out.println(person);
+        session.close();
+        in.close();
+
+
+
     }
 
 }
